13a94ecb5315d5d7646799921b714f7195de29a3
gheyderov <g.heyderov@gmail.com>
2022-09-21 11:25:51 +0400

    products

--- app.py
+++ app.py
@@ -15,6 +15,41 @@ users = {
     }
 }
 
+products = {
+    1 : {
+        "id" : 1,
+        "title" : 'Product#1',
+        "description" : "Some Description #1",
+        "price" : '100 USD',
+        "image" : "p1.avif"
+    },
+    2 : {
+        "id" : 2,
+        "title" : 'Product#2',
+        "description" : "Some Description #2",
+        "price" : '200 USD',
+        "image" : "p2.avif"
+    },
+    3 : {
+        "id" : 3,
+        "title" : 'Product#3',
+        "description" : "Some Description #3",
+        "price" : '300 USD',
+        "image" : "p3.avif"
+    }
+}
+
+
 @app.route("/home/")
 def home():
     return render_template('index.html', students = users)
+
+
+@app.route("/product_list/")
+def product_list():
+    return render_template('product_list.html', items = products)
+    
+
+@app.route("/product_detail/<int:id>/")
+def product_detail(id):
+    return render_template('product_detail.html', item = products[id])
